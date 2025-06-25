5803824ad36e6257963ade4e8f7f67d5f0379a58
JanaS2004 <717822T128@kce.ac.in>
2025-06-25 14:32:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+const on_btn = document.getElementById("on");
+const off_btn = document.getElementById("off");
+var img = document.getElementById("bulb");
+
+function bulb_on()
+{
+    img.src = "https://cdn.pixabay.com/photo/2013/07/12/19/20/bulb-154568_1280.png";
+
+}
+function bulb_off(){
+  img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKqnphEqfPBFe-17l6AQkkpoxgAU9mqkllCj2-BsTuDcaefCVphzDTtFnVl_-ezFb7VLs&usqp=CAU";
+}
